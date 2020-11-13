fe5c3458eb75a348edab7fa4e529f6cba168041c
`foxslot` <`foxslot777@gmail.com`>
2020-11-14 00:19:20 +0300

    add dark

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let light = document.querySelector(`#light`);
+let forest = document.querySelector(`#forest`);
+let dark = document.querySelector(`#dark`);
+
+forest.style.background = `none`;
 
 //переключатель фонаря
 on = false;
@@ -10,9 +13,11 @@ document.addEventListener('click', function (ev) {
     if (on) {
         let x = ev.pageX / window.innerWidth * 100;
         let y = ev.pageY / window.innerHeight * 100;
-        light.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent 160px, rgba(0,0,0,0.95) 200px)`;
+        forest.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent 160px, rgba(0,0,0,0.95) 200px)`;
+        dark.style.background = `none`;
     } else {
-        light.style.background = `none`;
+        forest.style.background = `none`;
+        dark.style.background = `black`;
     }
 });
 
@@ -21,6 +26,8 @@ document.addEventListener('mousemove', function (ev) {
     if (on) {
         let x = ev.pageX / window.innerWidth * 100;
         let y = ev.pageY / window.innerHeight * 100;
-        light.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent 160px, rgba(0,0,0,0.95) 200px)`;
+        forest.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent 160px, rgba(0,0,0,0.95) 200px)`;
+        dark.style.background = `none`;
     }
 })
+
